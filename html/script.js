f52d4d81afd3cb9e74045c970fbcd26bce183b2d
YukaChka <maxzerovol@gmail.com>
2023-02-06 20:07:50 +0300

    11/0
    1/0

---
+++ html/script.js
@@ -0,0 +1,8 @@
+
+function KvadratCAL(){
+    var result
+    var Kv1 = Number(document.getElementById("Kv1").value); 
+    var Kv2 = Number(document.getElementById("Kv2").value); 
+    result = Kv1 + Kv2
+    document.getElementById("result").value = result;
+}
