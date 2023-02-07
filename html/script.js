da96761877af41997bfc12a122afaeaf194578ee
YukaChka <maxzerovol@gmail.com>
2023-02-07 19:07:52 +0300

    update
    update

--- html/script.js
+++ html/script.js
@@ -6,12 +6,15 @@ function KvadratCAL(){
     var Kv3 = document.getElementById("Kv3").value;
 
     const D = Kv2**2-4 * Kv1 * Kv3 
-
+    console.log(D)
     document.getElementById("result").innerText = (parseInt(-Kv2,10)+parseInt(Math.sqrt(D),10))/(2*parseInt(Kv1,10));
     document.getElementById("result2").innerText = (parseInt(-Kv2,10)-parseInt(Math.sqrt(D),10))/(2*parseInt(Kv1,10));
   
 }   
 
 function SinCosTg(){
-    
+    var SKTValue = document.getElementById("selectvalueSKT").value;
+    if(SKTValue = 1){
+
+    }
 }
